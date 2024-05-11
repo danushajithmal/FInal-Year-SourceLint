12219cef98b76dcc2aec6c92ed11cbc8418adb63
danushajithmal <124653685+danushajithmal@users.noreply.github.com>
2024-05-11 19:42:12 +0530

    Url Fixed

--- Test Site/src/pages/test.js
+++
@@ -1,8 +0,0 @@
-app.post('/update-email', function (req, res) {
-
-    // updates the email based on POST data
-
-    user.email = req.body.email;
-
-    res.send("Email updated successfully!");
-});
